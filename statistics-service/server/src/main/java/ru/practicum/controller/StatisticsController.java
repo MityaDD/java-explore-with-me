2f4f2b2e6a31426845bf5eb023231f251b934e42
Mitya <ndvorkun@yandex.ru>
2023-07-12 22:27:56 +0300

    fix properties

--- statistics-service/server/src/main/java/ru/practicum/controller/StatisticsController.java
+++ statistics-service/server/src/main/java/ru/practicum/controller/StatisticsController.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @Slf4j
-public class Controller {
+public class StatisticsController {
     private final StatService service;
     private static final String PATTERN = "yyyy-MM-dd HH:mm:ss";
 
@@ -28,10 +28,10 @@ public class Controller {
     }
 
     @GetMapping("/stats")
-    public List<HitDTO> getHit(@RequestParam(name = "start") @DateTimeFormat(pattern = PATTERN) LocalDateTime start,
-                               @RequestParam(name = "end") @DateTimeFormat(pattern = PATTERN) LocalDateTime end,
-                               @RequestParam(name = "uris", required = false, defaultValue = "") List<String> uris,
-                               @RequestParam(name = "unique", required = false, defaultValue = "false") Boolean unique) {
+    public List<HitDTO> getHit(@RequestParam @DateTimeFormat(pattern = PATTERN) LocalDateTime start,
+                               @RequestParam @DateTimeFormat(pattern = PATTERN) LocalDateTime end,
+                               @RequestParam(required = false, defaultValue = "") List<String> uris,
+                               @RequestParam(required = false, defaultValue = "false") Boolean unique) {
         log.info("Запрошена статистика по посещениям с {} по {}", start, end);
         return service.getStats(start, end, uris, unique);
     }
