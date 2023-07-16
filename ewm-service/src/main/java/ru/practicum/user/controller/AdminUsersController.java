aca25c96d9147d4858bb073750e8018847e616c6
Mitya <ndvorkun@yandex.ru>
2023-07-16 16:21:27 +0300

    fix docker

---
+++ ewm-service/src/main/java/ru/practicum/user/controller/AdminUsersController.java
@@ -0,0 +1,45 @@
+package ru.practicum.user.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.user.dto.NewUserRequest;
+import ru.practicum.user.dto.UserDto;
+import ru.practicum.user.service.UserServiceImpl;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/admin/users")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AdminUsersController {
+    final UserServiceImpl userService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    List<UserDto> getUsers(@RequestParam(name = "ids", required = false) List<Long> ids,
+                           @RequestParam(defaultValue = "0") @PositiveOrZero Integer from,
+                           @RequestParam(defaultValue = "10") @Positive Integer size) {
+        return userService.getUsers(ids, from, size);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    UserDto createUser(@RequestBody @Valid NewUserRequest userRequest) {
+        return userService.createUser(userRequest);
+    }
+
+    @DeleteMapping("/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+    }
+}
