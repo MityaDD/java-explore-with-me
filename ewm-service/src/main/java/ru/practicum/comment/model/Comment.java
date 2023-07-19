912c9f42fa4145578a44232baa075c49bee96648
Mitya <ndvorkun@yandex.ru>
2023-07-19 17:40:34 +0300

    add model

---
+++ ewm-service/src/main/java/ru/practicum/comment/model/Comment.java
@@ -0,0 +1,31 @@
+package ru.practicum.comment.model;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import ru.practicum.event.model.Event;
+import ru.practicum.user.model.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @ManyToOne
+    User author;
+    @ManyToOne
+    Event event;
+    LocalDateTime created;
+    @Enumerated(EnumType.STRING)
+    CommentStatus status;
+    String text;
+}
